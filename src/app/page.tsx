c17ec7d0f0e30793bf09e156b2117765cb347072
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-01-18 03:47:23 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 import Footer from "@/components/Footer";
-import Aboutus from "@/components/HomeScreen/Aboutus";
+// import Aboutus from "@/components/HomeScreen/Aboutus";
 import Ecosystem from "@/components/HomeScreen/Ecosystem";
 import FAQSection from "@/components/HomeScreen/FAQSection";
 import Hero from "@/components/HomeScreen/Hero";
 import Sliderb from "@/components/HomeScreen/Sliderb";
-import WhySeedai from "@/components/HomeScreen/WhySeedai";
+// import WhySeedai from "@/components/HomeScreen/WhySeedai";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
