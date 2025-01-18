6cd8d03d13f4a362156ab242e58d96c2e7f44b0c
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-01-18 17:33:44 +0530

    Update Ecosystem.tsx

--- src/components/HomeScreen/Ecosystem.tsx
+++ src/components/HomeScreen/Ecosystem.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaDiscord } from "react-icons/fa6";
+import { FaTelegramPlane } from "react-icons/fa";
+import { RiMediumFill } from "react-icons/ri";
 
 function Ecosystem() {
   return (
     <section className=" text-white py-16 my-2 sm:my-10 relative max-w-7xl mx-auto  ">
-   
-    
       <div className="container mx-auto px-4 z-10">
         {/* Section Heading */}
         <h2 className="text-center text-3xl md:text-4xl font-bold mb-12">
@@ -16,22 +18,40 @@ function Ecosystem() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
           {/* Card 1 */}
           <div className="">
-            <div className="box2 rounded-lg border border-purple-600 p-6 shadow-lg hover:scale-105 transform transition duration-300">
+            <div className="box2 rounded-lg border border-purple-600 p-6 shadow-lg hover:scale-105 transform transition duration-300 ">
               <div className="flex flex-col sm:flex-row gap-4">
                 <div className="flex gap-2">
-               <img src="images/mod.png" alt="" className="mx-auto" />
-               <div>
-               <h3 className="text-2xl font-bold text-purple-400 mb-4">
-                    AI Marketplace
-                  </h3>
-                  <p className="text-sm text-gray-300">
-                    Discover pre-vetted, reliable AI agents to automate and
-                    optimize your processes.
-                  </p>
-               </div>
+                  <img src="images/mod.png" alt="" className="mx-auto" />
+                  <div>
+                    <h3 className="text-2xl font-bold text-purple-400 mb-4">
+                      AI Marketplace
+                    </h3>
+                    <p className="text-sm text-gray-300">
+                      Discover pre-vetted, reliable AI agents to automate and
+                      optimize your processes.
+                    </p>
+                  </div>
                 </div>
                 <div>
-                  <img src="images/web.svg" alt="" className="h-[150px] w-auto sm:h-auto mx-auto" />
+                  <div className="relative">
+                    <div>
+                      <h2 className="bg-[#ffffff1f] px-2 py-1 rounded text-sm font-eculid text-center absolute top-[-10%] right-0">
+                        AGENTS
+                      </h2>
+                    </div>
+                    <div className="flex  bottom-0 space-x-6 text-gray-500 pb-2 pt-24">
+                      <a href="#" className="hover:text-white">
+                        <FaXTwitter />
+                      </a>
+                      <a href="#" className="hover:text-white">
+                        <FaDiscord />
+                      </a>
+                      <a href="#" className="hover:text-white">
+                        <FaTelegramPlane />
+                      </a>
+                  
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -39,22 +59,40 @@ function Ecosystem() {
 
           {/* Card 2 */}
           <div className="">
-            <div className="box2 rounded-lg border border-purple-600 p-6 shadow-lg hover:scale-105 transform transition duration-300">
+            <div className="box2 rounded-lg border border-purple-600 p-6 shadow-lg hover:scale-105 transform transition duration-300 ">
               <div className="flex flex-col sm:flex-row gap-4">
                 <div className="flex gap-2">
-               <img src="images/mod.png" alt="" className="mx-auto" />
-               <div>
-               <h3 className="text-2xl font-bold text-purple-400 mb-4">
-                    AI Marketplace
-                  </h3>
-                  <p className="text-sm text-gray-300">
-                    Discover pre-vetted, reliable AI agents to automate and
-                    optimize your processes.
-                  </p>
-               </div>
+                  <img src="images/mod.png" alt="" className="mx-auto" />
+                  <div>
+                    <h3 className="text-2xl font-bold text-purple-400 mb-4">
+                      AI Marketplace
+                    </h3>
+                    <p className="text-sm text-gray-300">
+                      Discover pre-vetted, reliable AI agents to automate and
+                      optimize your processes.
+                    </p>
+                  </div>
                 </div>
                 <div>
-                  <img src="images/web.svg" alt="" className="h-[150px] w-auto sm:h-auto mx-auto" />
+                  <div className="relative">
+                    <div>
+                      <h2 className="bg-[#ffffff1f] px-2 py-1 rounded text-sm font-eculid text-center absolute top-[-10%] right-0">
+                        AGENTS
+                      </h2>
+                    </div>
+                    <div className="flex  bottom-0 space-x-6 text-gray-500 pb-2 pt-24">
+                      <a href="#" className="hover:text-white">
+                        <FaXTwitter />
+                      </a>
+                      <a href="#" className="hover:text-white">
+                        <FaDiscord />
+                      </a>
+                      <a href="#" className="hover:text-white">
+                        <FaTelegramPlane />
+                      </a>
+                  
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -62,22 +100,40 @@ function Ecosystem() {
 
           {/* Card 3 */}
           <div className="">
-            <div className="box2 rounded-lg border border-purple-600 p-6 shadow-lg hover:scale-105 transform transition duration-300">
+            <div className="box2 rounded-lg border border-purple-600 p-6 shadow-lg hover:scale-105 transform transition duration-300 ">
               <div className="flex flex-col sm:flex-row gap-4">
                 <div className="flex gap-2">
-               <img src="images/mod.png" alt="" className="mx-auto" />
-               <div>
-               <h3 className="text-2xl font-bold text-purple-400 mb-4">
-                    AI Marketplace
-                  </h3>
-                  <p className="text-sm text-gray-300">
-                    Discover pre-vetted, reliable AI agents to automate and
-                    optimize your processes.
-                  </p>
-               </div>
+                  <img src="images/mod.png" alt="" className="mx-auto" />
+                  <div>
+                    <h3 className="text-2xl font-bold text-purple-400 mb-4">
+                      AI Marketplace
+                    </h3>
+                    <p className="text-sm text-gray-300">
+                      Discover pre-vetted, reliable AI agents to automate and
+                      optimize your processes.
+                    </p>
+                  </div>
                 </div>
                 <div>
-                  <img src="images/web.svg" alt="" className="h-[150px] w-auto sm:h-auto mx-auto" />
+                  <div className="relative">
+                    <div>
+                      <h2 className="bg-[#ffffff1f] px-2 py-1 rounded text-sm font-eculid text-center absolute top-[-10%] right-0">
+                        AGENTS
+                      </h2>
+                    </div>
+                    <div className="flex  bottom-0 space-x-6 text-gray-500 pb-2 pt-24">
+                      <a href="#" className="hover:text-white">
+                        <FaXTwitter />
+                      </a>
+                      <a href="#" className="hover:text-white">
+                        <FaDiscord />
+                      </a>
+                      <a href="#" className="hover:text-white">
+                        <FaTelegramPlane />
+                      </a>
+                  
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -85,22 +141,123 @@ function Ecosystem() {
 
           {/* Card 4 */}
           <div className="">
-            <div className="box2 rounded-lg border border-purple-600 p-6 shadow-lg hover:scale-105 transform transition duration-300">
+            <div className="box2 rounded-lg border border-purple-600 p-6 shadow-lg hover:scale-105 transform transition duration-300 ">
               <div className="flex flex-col sm:flex-row gap-4">
                 <div className="flex gap-2">
-               <img src="images/mod.png" alt="" className="mx-auto" />
-               <div>
-               <h3 className="text-2xl font-bold text-purple-400 mb-4">
-                    AI Marketplace
-                  </h3>
-                  <p className="text-sm text-gray-300">
-                    Discover pre-vetted, reliable AI agents to automate and
-                    optimize your processes.
-                  </p>
-               </div>
+                  <img src="images/mod.png" alt="" className="mx-auto" />
+                  <div>
+                    <h3 className="text-2xl font-bold text-purple-400 mb-4">
+                      AI Marketplace
+                    </h3>
+                    <p className="text-sm text-gray-300">
+                      Discover pre-vetted, reliable AI agents to automate and
+                      optimize your processes.
+                    </p>
+                  </div>
                 </div>
                 <div>
-                  <img src="images/web.svg" alt="" className="h-[150px] w-auto sm:h-auto mx-auto" />
+                  <div className="relative">
+                    <div>
+                      <h2 className="bg-[#ffffff1f] px-2 py-1 rounded text-sm font-eculid text-center absolute top-[-10%] right-0">
+                        AGENTS
+                      </h2>
+                    </div>
+                    <div className="flex  bottom-0 space-x-6 text-gray-500 pb-2 pt-24">
+                      <a href="#" className="hover:text-white">
+                        <FaXTwitter />
+                      </a>
+                      <a href="#" className="hover:text-white">
+                        <FaDiscord />
+                      </a>
+                      <a href="#" className="hover:text-white">
+                        <FaTelegramPlane />
+                      </a>
+                  
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="">
+            <div className="box2 rounded-lg border border-purple-600 p-6 shadow-lg hover:scale-105 transform transition duration-300 ">
+              <div className="flex flex-col sm:flex-row gap-4">
+                <div className="flex gap-2">
+                  <img src="images/mod.png" alt="" className="mx-auto" />
+                  <div>
+                    <h3 className="text-2xl font-bold text-purple-400 mb-4">
+                      AI Marketplace
+                    </h3>
+                    <p className="text-sm text-gray-300">
+                      Discover pre-vetted, reliable AI agents to automate and
+                      optimize your processes.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <div className="relative">
+                    <div>
+                      <h2 className="bg-[#ffffff1f] px-2 py-1 rounded text-sm font-eculid text-center absolute top-[-10%] right-0">
+                        AGENTS
+                      </h2>
+                    </div>
+                    <div className="flex  bottom-0 space-x-6 text-gray-500 pb-2 pt-24">
+                      <a href="#" className="hover:text-white">
+                        <FaXTwitter />
+                      </a>
+                      <a href="#" className="hover:text-white">
+                        <FaDiscord />
+                      </a>
+                      <a href="#" className="hover:text-white">
+                        <FaTelegramPlane />
+                      </a>
+                  
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div className="">
+            <div className="box2 rounded-lg border border-purple-600 p-6 shadow-lg hover:scale-105 transform transition duration-300 ">
+              <div className="flex flex-col sm:flex-row gap-4">
+                <div className="flex gap-2">
+                  <img src="images/mod.png" alt="" className="mx-auto" />
+                  <div>
+                    <h3 className="text-2xl font-bold text-purple-400 mb-4">
+                      AI Marketplace
+                    </h3>
+                    <p className="text-sm text-gray-300">
+                      Discover pre-vetted, reliable AI agents to automate and
+                      optimize your processes.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <div className="relative">
+                    <div>
+                      <h2 className="bg-[#ffffff1f] px-2 py-1 rounded text-sm font-eculid text-center absolute top-[-10%] right-0">
+                        AGENTS
+                      </h2>
+                    </div>
+                    <div className="flex  bottom-0 space-x-6 text-gray-500 pb-2 pt-24">
+                      <a href="#" className="hover:text-white">
+                        <FaXTwitter />
+                      </a>
+                      <a href="#" className="hover:text-white">
+                        <FaDiscord />
+                      </a>
+                      <a href="#" className="hover:text-white">
+                        <FaTelegramPlane />
+                      </a>
+                  
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
