a83354217139c5382435151452ebd172224cdd20
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-01-18 17:24:00 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,31 +5,31 @@ import { FaDiscord } from "react-icons/fa6";
 import { FaTelegramPlane } from "react-icons/fa";
 import { RiMediumFill } from "react-icons/ri";
 
-
-
 const Footer: React.FC = () => {
   return (
     <footer className=" text-white pt-12   relative ">
-      <img src="images/footright.png" alt="" className=" absolute top-0 right-0 h-[70px]"/>
+      <img
+        src="images/footright.png"
+        alt=""
+        className=" absolute top-0 right-0 h-[70px]"
+      />
       <div className="container mx-auto px-4">
         {/* Top Section */}
-      
 
         {/* Links Section */}
         <div className="">
           {/* Logo */}
           <div>
             <div className="text-2xl font-bold mb-4 flex items-center">
-          
-              <img src="images/logo.png" alt="" className=" w-[200px] mx-auto"/>
+              <img
+                src="images/logo.png"
+                alt=""
+                className=" w-[200px] mx-auto"
+              />
             </div>
           </div>
 
           {/* Column Links */}
-    
- 
-   
-
         </div>
 
         {/* Bottom Section */}
@@ -38,14 +38,26 @@ const Footer: React.FC = () => {
             © Copyright SEED AI 2024
           </p>
           <div className="flex space-x-6 text-gray-500 pb-2">
-            <a href="#" className="hover:text-white"><FaXTwitter/></a>
-            <a href="#" className="hover:text-white"><FaDiscord/></a>
-            <a href="#" className="hover:text-white"><FaTelegramPlane/></a>
-            <a href="#" className="hover:text-white"><RiMediumFill/></a>
+            <a href="#" className="hover:text-white">
+              <FaXTwitter />
+            </a>
+            <a href="#" className="hover:text-white">
+              <FaDiscord />
+            </a>
+            <a href="#" className="hover:text-white">
+              <FaTelegramPlane />
+            </a>
+            <a href="#" className="hover:text-white">
+              <RiMediumFill />
+            </a>
           </div>
         </div>
       </div>
-      <img src="images/footleft.png" alt="" className=" absolute bottom-0 left-0 h-[90px]"/>
+      <img
+        src="images/footleft.png"
+        alt=""
+        className=" absolute bottom-0 left-0 h-[90px]"
+      />
     </footer>
   );
 };
